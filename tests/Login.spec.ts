adac563ba635100fba580a3941833c4026e7016c
user <sergei.zahharov1@gmail.com>
2025-01-31 12:11:19 +0200

    HW-11 | add jwt verification and negative test

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -8,5 +8,18 @@ test.describe('Login tests', async () => {
       data: LoginDTO.createLoginWithCorrectData(),
     })
     expect(response.status()).toBe(StatusCodes.OK)
+
+    const responseBody = await response.text()
+    const jwtRegex = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
+    expect(responseBody).toMatch(jwtRegex)
+  })
+
+  test('Request with incorrect HTTP method should return METHOD_NOT_ALLOWED status', async ({
+    request,
+  }) => {
+    const response = await request.get('https://backend.tallinn-learning.ee/login/student', {
+      data: LoginDTO.createLoginWithCorrectData(),
+    })
+    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
   })
 })
